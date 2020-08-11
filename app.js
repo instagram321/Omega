b949010659dd8ac5ad8ee4a7b1671fb677b429f4
instagram321 <69516580+instagram321@users.noreply.github.com>
2020-08-11 20:52:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB71eP8940m7nUe9CF53vn0KBZ4o6OZkS8",
+  authDomain: "insta-1242a.firebaseapp.com",
+  databaseURL: "https://insta-1242a.firebaseio.com",
+  projectId: "insta-1242a",
+  storageBucket: "insta-1242a.appspot.com",
+  messagingSenderId: "142420520116",
+  appId: "1:142420520116:web:f994c5f988214a8072fbe1",
+  measurementId: "G-K834BYEYDQ"
 };
 
      
